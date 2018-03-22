06c372934a956482a02c4e359d2d0126641e3913
Leulz <leo.vital@ccc.ufcg.edu.br>
2018-03-21 22:11:05 -0300

    Decoding JWT header

--- src/RS256.hs
+++ src/RS256.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -9,12 +8,12 @@
 
 module RS256 where
 
-import Import.NoFoundation
-
+import qualified Data.Text as T
 import Data.String (fromString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Base64.URL as BS64
+import qualified Data.ByteString.Char8 as C8
 
 import Data.X509 (SignedCertificate, PubKey(PubKeyRSA), certPubKey, getCertificate)
 import Data.X509.Memory (readSignedObjectFromMemory)
@@ -28,8 +27,9 @@ import Control.Lens
 import qualified Data.Aeson.Lens as DAL (_String, key)
 import Data.Map as Map
 import Data.Aeson (Value)
+import Data.List.Split
 
-type Resp = Response (Map String Value)
+import qualified Web.JWT as JWT
 
 readSignedCertificateFromMemory :: B.ByteString -> [SignedCertificate]
 readSignedCertificateFromMemory = readSignedObjectFromMemory
@@ -40,21 +40,50 @@ extractRSA _ = error "Error!"
 extractPubKey (x:[]) = extractRSA $ certPubKey $  getCertificate x
 extractPubKey _ = error "Error!"
 
+addPadding :: String -> String
+addPadding bs64string = bs64string ++ (Prelude.concat $ Prelude.replicate (4 - ((length bs64string) `mod` 4)) "=")
+
+getFirst :: [[Char]] -> [Char]
+getFirst (x:_) = x
+getFirst _ = error "Invalid JWT!"
+
+getHeaderWithPadding :: String -> String
+getHeaderWithPadding jwt = if (length jwt) == 0 then error "Invalid JWT!" else addPadding $ getFirst $ splitOn "." jwt
+
+-- data JWTHeader = JWTHeader {
+--     kid :: Maybe Text
+--   , alg :: Maybe Text
+-- } deriving (Eq, Show)
+
+-- instance FromJSON JWTHeader where
+--     parseJSON = withObject "JWTHeader"
+--                     (\o -> JWTHeader
+--                     <$> o .:? "kid"
+--                     <*> o .:? "alg")
+
+-- instance ToJSON JWTHeader where
+--     toJSON JWTHeader{..} = object $ catMaybes [
+--                   fmap ("kid" .=) kid
+--                 , fmap ("alg" .=) alg
+--             ]
+
 testRS256 :: IO (Bool)
 testRS256 = do
+    let jwt = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImM2ZjBlZTE2YmU3MGM0ODhkZDM5ZGI3MGY2ZjRkMTM3YTA0ODkxZTMifQ.eyJhenAiOiI1ODI0MzczNzE4NDctdHJza2hubWdzaXFtcmZyZGJ1cmxlZG44anVvMWVzc3QuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI1ODI0MzczNzE4NDctdHJza2hubWdzaXFtcmZyZGJ1cmxlZG44anVvMWVzc3QuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDYzMjAxODYzMDAzNDQ1ODcyNzEiLCJoZCI6ImNjYy51ZmNnLmVkdS5iciIsImVtYWlsIjoibGVvLnZpdGFsQGNjYy51ZmNnLmVkdS5iciIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoibGxKRGFlcHVVYlFteVBwWS1lbnZQUSIsImV4cCI6MTUyMTYwMDQ2MywiaXNzIjoiYWNjb3VudHMuZ29vZ2xlLmNvbSIsImp0aSI6IjhmMTFkNDU5MTgwZmI3NTExY2UwNDc2Zjc4NmFlNDU1YzJjMTVjOTAiLCJpYXQiOjE1MjE1OTY4NjMsIm5hbWUiOiJMZW8gVml0YWwiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDUuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1MSXBEbE1Bb19hcy9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BR2k0Z2Z4ZmlhTFo3OEdZYUJIZTY0djBmMlBndElXRDZnL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJMZW8iLCJmYW1pbHlfbmFtZSI6IlZpdGFsIiwibG9jYWxlIjoicHQifQ.P4x8zyDn3jE5MUqvetVVTQOKj_GUIvL1leRxxgZ-_NPWSgA3dB6BRJNPLKwlMXTKtBThD5iUmDiJ6NqdjMI5WVbkBHmVeh5ZmnzqO-9RGq_Di72fSiz60p4jMzDOVmvcfnzcLVob0S3flKWSwflxGz3Wkce2-yyeJX1HEMQYPtO6f8I_-w43cgnL8J1dJPpOhbxF-m9RYUuI6Xhnk_ZeOMZr9q1CZa5tJmsepfYsRikluNXLaMzMvN0TcQvunbjR3ZXTY10Sw6cv4CKCi_CsIMdKlDRu1GodmlLelx6lg26s9j2PFzwOCgzBWf8fXHcISN0X5S_0pPxrDHjA_3lytA"
+    h <- return $ BS64.decode $ C8.pack $ getHeaderWithPadding jwt
     r <- WREQ.get "https://www.googleapis.com/oauth2/v1/certs"
     certificate <- return $ r ^? WREQ.responseBody . DAL.key "c6f0ee16be70c488dd39db70f6f4d137a04891e3" . DAL._String
     case certificate of
         Just cert -> do
-            let rsaCertificate = fromString $ unpack cert
+            let rsaCertificate = fromString $ T.unpack cert
             let jwtToken = fromString $ "eyJhbGciOiJSUzI1NiIsImtpZCI6ImM2ZjBlZTE2YmU3MGM0ODhkZDM5ZGI3MGY2ZjRkMTM3YTA0ODkxZTMifQ.eyJhenAiOiI1ODI0MzczNzE4NDctdHJza2hubWdzaXFtcmZyZGJ1cmxlZG44anVvMWVzc3QuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI1ODI0MzczNzE4NDctdHJza2hubWdzaXFtcmZyZGJ1cmxlZG44anVvMWVzc3QuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDYzMjAxODYzMDAzNDQ1ODcyNzEiLCJoZCI6ImNjYy51ZmNnLmVkdS5iciIsImVtYWlsIjoibGVvLnZpdGFsQGNjYy51ZmNnLmVkdS5iciIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoibGxKRGFlcHVVYlFteVBwWS1lbnZQUSIsImV4cCI6MTUyMTYwMDQ2MywiaXNzIjoiYWNjb3VudHMuZ29vZ2xlLmNvbSIsImp0aSI6IjhmMTFkNDU5MTgwZmI3NTExY2UwNDc2Zjc4NmFlNDU1YzJjMTVjOTAiLCJpYXQiOjE1MjE1OTY4NjMsIm5hbWUiOiJMZW8gVml0YWwiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDUuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1MSXBEbE1Bb19hcy9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BR2k0Z2Z4ZmlhTFo3OEdZYUJIZTY0djBmMlBndElXRDZnL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJMZW8iLCJmYW1pbHlfbmFtZSI6IlZpdGFsIiwibG9jYWxlIjoicHQifQ"
             let jwtSignature = fromString $ "P4x8zyDn3jE5MUqvetVVTQOKj_GUIvL1leRxxgZ-_NPWSgA3dB6BRJNPLKwlMXTKtBThD5iUmDiJ6NqdjMI5WVbkBHmVeh5ZmnzqO-9RGq_Di72fSiz60p4jMzDOVmvcfnzcLVob0S3flKWSwflxGz3Wkce2-yyeJX1HEMQYPtO6f8I_-w43cgnL8J1dJPpOhbxF-m9RYUuI6Xhnk_ZeOMZr9q1CZa5tJmsepfYsRikluNXLaMzMvN0TcQvunbjR3ZXTY10Sw6cv4CKCi_CsIMdKlDRu1GodmlLelx6lg26s9j2PFzwOCgzBWf8fXHcISN0X5S_0pPxrDHjA_3lytA==" --Add padding with ==
             let signed = readSignedCertificateFromMemory rsaCertificate
             let pubk = extractPubKey signed
-            let decodedSig = BS64.decode $ toStrict jwtSignature
+            let decodedSig = BS64.decode $ BS.toStrict jwtSignature
             case decodedSig of
                 Right bytes -> do
-                    let ver = verify pubk jwtToken (fromStrict bytes)
+                    let ver = verify pubk jwtToken (BS.fromStrict bytes)
                     return ver
                 Left _ -> return False
         Nothing -> return False
